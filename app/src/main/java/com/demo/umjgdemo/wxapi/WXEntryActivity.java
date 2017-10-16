e2b2f16ee9ba26e932c4d559073fdc77d2aa0ece
wangxin31199 <79957843@qq.com>
2017-10-16 17:39:26 +0800

    第一次提交代码

---
+++ app/src/main/java/com/demo/umjgdemo/wxapi/WXEntryActivity.java
@@ -0,0 +1,12 @@
+package com.demo.umjgdemo.wxapi;
+
+
+import com.umeng.weixin.callback.WXCallbackActivity;
+
+//import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+
+}
